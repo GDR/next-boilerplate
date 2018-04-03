e08015bd37d77a1a262b29ece02c254fcbbe6eee
Damir Garifullin <gosugdr@gmail.com>
2018-04-03 17:32:33 +0300

    Fixed eslint. Changed to airbnb

--- src/lib/configureStore.dev.js
+++ src/lib/configureStore.dev.js
@@ -1,6 +1,6 @@
 import { createStore } from 'redux';
-import { reducers } from '../reducers';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import reducers from '../reducers';
 
 export default function configureStore(initialState) {
   return createStore(reducers, initialState, composeWithDevTools());
--- src/lib/configureStore.prod.js
+++ src/lib/configureStore.prod.js
@@ -1,5 +1,5 @@
 import { createStore } from 'redux';
-import { reducers } from '../reducers';
+import reducers from '../reducers';
 
 export default function configureStore(initialState) {
   return createStore(reducers, initialState);
